cf17577895c3c0fe958b0dc5e10278537b0103b9
ManojBudumuru <manoj.budumuru@vungle.com>
2024-05-01 14:12:09 -0700

    SDK version Upgrade to 7.3.2
    Bug fixes and improvements
    
    IOS-6715

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
                     name: "VungleAdsSDK",
-                    url: "https://vungle2-sdk-dev-scratch.s3.amazonaws.com/ios/SwiftPackageManager/VungleAds-7.3.0.zip",
-                    checksum: "8c973d4d4d96786939e8a52bc5fcf85ceaef95af7e265bbfa948b5ad85d43ff0"
+                    url: "https://vungle2-sdk-dev-scratch.s3.amazonaws.com/ios/SwiftPackageManager/VungleAds-7.3.2.zip",
+                    checksum: "73da4fac4af46dc2416f92f4646439da475dd840745e365f72ec6b5d2538ae87"
         )
     ]
 )
