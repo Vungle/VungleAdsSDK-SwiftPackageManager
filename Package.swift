73e7f5cb0541f2040ca5e4fe17c159f18202ea6d
crbinsf <crbinsf@users.noreply.github.com>
2026-04-14 17:07:18 -0700

    SwiftPackageManager updates for 7.7.2 release (#32)

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
                     name: "VungleAdsSDK",
-                    url: "https://vungle2-cdn-prod.s3.amazonaws.com/sdks/ios/SwiftPackageManager/VungleAds-7.7.1.zip",
-                    checksum: "dca1a9084f4175ab2485046eb140c831b38cb6a35e36212b72691d119effbf75"
+                    url: "https://vungle2-cdn-prod.s3.amazonaws.com/sdks/ios/SwiftPackageManager/VungleAds-7.7.2.zip",
+                    checksum: "992601fbf1bea916c14c898d6686800b53344f766ee45606fb91088d7a0eafa9"
         )
     ]
 )
