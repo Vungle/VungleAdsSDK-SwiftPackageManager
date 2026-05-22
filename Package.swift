0d31ac2833b278c9c3fbeceabe28cdec0a2e18cf
Akifumi Shinagawa <ashinagawa@liftoff.io>
2026-05-22 15:07:23 -0700

    Added static framework support

--- Package.swift
+++ Package.swift
@@ -10,12 +10,20 @@ let package = Package(
         .library(
             name: "VungleAdsSDK",
             targets: ["VungleAdsSDK"]),
+        .library(
+            name: "VungleAdsSDKStatic",
+            targets: ["VungleAdsSDKStatic"]),
     ],
     targets: [
         .binaryTarget(
                     name: "VungleAdsSDK",
                     url: "https://vungle2-cdn-prod.s3.amazonaws.com/sdks/ios/SwiftPackageManager/VungleAds-7.7.3.zip",
                     checksum: "aba2b6ae895521b46ce3499fed27821822f04e2e5cec57b20bb99a598e7ea3c3"
+        ),
+        .binaryTarget(
+                    name: "VungleAdsSDKStatic",
+                    url: "https://vungle2-cdn-prod.s3.amazonaws.com/sdks/ios/SwiftPackageManager/VungleAds-7.7.3-static.zip",
+                    checksum: "d300a5ced3504f0538b9675f542d206a13202b315f11478cda31b12e4a4029f4"
         )
     ]
 )
